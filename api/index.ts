51d8b56ffde8c629ec95e8f5906d0c2047c89d86
Rob <kritz.rob@gmail.com>
2025-02-19 20:16:44 +0200

    add a route

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/testroute", (req, res) => res.send("heres a route bru"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
